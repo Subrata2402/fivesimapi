6e3474ec4668b8b23c0c4af3a83c8b1832a8518f
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-13 14:28:51 +0530

    update fivesim.py

--- FiveSimApi/fivesim.py
+++ FiveSimApi/fivesim.py
@@ -2,7 +2,7 @@ import aiohttp, asyncio, json, requests, bs4
 
 class NumberApi(object):
     def __init__(self, api_key: str = None):
-        self.api_key = "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODM5NDQ2OTIsImlhdCI6MTY1MjQwODY5MiwicmF5IjoiNmU3NWM0NzE1NjRmMjA1ZTlmNzZhNWYyYzkzMzlmYTIiLCJzdWIiOjEwNzcyODJ9.RxRJ2vxrwpw6J0BY0SnduNAXn3wnxxsNBlH-gGMsYZIW9YgUzc7L3TI13p3Pbqf9CR8rueHG0sWklZ9Xl_gYLyBa6_xNi0FTsnRo-wQiuOnuCeDtPjiqfiS_Q8ejt-VeTWKuhDJb8LqjVtilUmPPJIz2zId6LNjxaFnDWx_wq1lAtFyjyz4U2Ih4SQ9NaDlYqstsFfPRA8PRruHGCf9NKUIo-4oAfHb-UF1fBprio5FxRf6XBZ0F86b9tPA60xp5lGGNA8HWhbNA5IvH4N3erTijBjiJAx-CStsxB--xzPkMkBRlUkGEvlvw8xwGThyoGew1LaWAgSpqJRNSPMadEw"
+        self.api_key = api_key
         self.api_url = "https://5sim.net/v1"
         
     async def fetch(self, method = "GET", function = "", params = None, headers = None, data = None):
@@ -79,3 +79,9 @@ class NumberApi(object):
     async def sms_inbox_list(self, id):
         return await self.fetch("GET", "/user/sms/inbox/{}".format(id))
         
+    async def crypto_rates(self):
+        return await self.fetch("GET", "/user/payment/crypto2/rates")
+        
+    async def get_deposit_address(self, amount: str, currency: str, address_type: str = None):
+        data = 
+        return await self.fetch("POST", "/user/payment/crypto2/invoice", data = data)
