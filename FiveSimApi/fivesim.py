e9a73f60ff376f512782221d123af6d82f0f1b02
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-13 13:20:31 +0530

    update fivesim.py

--- FiveSimApi/fivesim.py
+++ FiveSimApi/fivesim.py
@@ -1,7 +1,7 @@
 import aiohttp, asyncio, json, requests, bs4
 
 class NumberApi(object):
-    def __init__(self):
+    def __init__(self, api_key: str = None):
         self.api_key = "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODM5NDQ2OTIsImlhdCI6MTY1MjQwODY5MiwicmF5IjoiNmU3NWM0NzE1NjRmMjA1ZTlmNzZhNWYyYzkzMzlmYTIiLCJzdWIiOjEwNzcyODJ9.RxRJ2vxrwpw6J0BY0SnduNAXn3wnxxsNBlH-gGMsYZIW9YgUzc7L3TI13p3Pbqf9CR8rueHG0sWklZ9Xl_gYLyBa6_xNi0FTsnRo-wQiuOnuCeDtPjiqfiS_Q8ejt-VeTWKuhDJb8LqjVtilUmPPJIz2zId6LNjxaFnDWx_wq1lAtFyjyz4U2Ih4SQ9NaDlYqstsFfPRA8PRruHGCf9NKUIo-4oAfHb-UF1fBprio5FxRf6XBZ0F86b9tPA60xp5lGGNA8HWhbNA5IvH4N3erTijBjiJAx-CStsxB--xzPkMkBRlUkGEvlvw8xwGThyoGew1LaWAgSpqJRNSPMadEw"
         self.api_url = "https://5sim.net/v1"
         
