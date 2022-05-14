6b6723c69014c9d818e667463eb7e4d69c6f12a6
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-14 14:08:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fivesimapi",
-    version="0.1",
+    version="0.1.1",
     author="Subrata",
     author_email="",
     description="Five sim api",
